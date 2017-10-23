a02bbf5514c0da82ec9a93d10a03b9807a6e4289
Muhammed Mohsen <muhammedmohsen@kernel.blue>
2017-10-23 17:13:53 +0200

    Update Service Worker

--- scripts/sw.js
+++ scripts/sw.js
@@ -1,35 +1,48 @@
+/**
+ * @file
+ * Serviceworker file for browser push notification.
+ */
 self.addEventListener('push', function (event) {
   'use strict';
-    if (!(self.Notification && self.Notification.permission === 'granted')) {
-        return;
-    }
-    var data = {};
-
-    const sendNotification = payload => {
-        const title = payload.title;
-        var message = payload.message;
-
-        return self.registration.showNotification(title, {
-          body: message,
-          icon: 'images/icon.png',
-          badge: 'images/badge.png',
-          lang: 'AR'
-        });
+  if (!(self.Notification && self.Notification.permission === 'granted')) {
+    return;
+  }
+// console.log('push received');
+  const sendNotification = body => {
+    var str = body;
+    var message_array = str.split('<br>');
+    var notificationOptions = {
+      body: message_array[1],
+      icon: message_array[2],
+      badge: message_array[2],
+      tag: message_array[2],
+      data: {
+        url: message_array[3]
+      }
     };
+    return self.registration.showNotification(message_array[0],
+            notificationOptions);
+  };
 
-    if (event.data) {
-         data = event.data.json();
-
-        event.waitUntil(sendNotification(data));
-    }
+  if (event.data) {
+    const message = event.data.text();
+    event.waitUntil(sendNotification(message));
+  }
+});
+self.addEventListener('notificationclick', function (event) {
+  'use strict';
+  event.notification.close();
+  // console.log('notification click');
+  Promise.resolve();
+  if (event.notification.data && event.notification.data.url) {
+    clients.openWindow(event.notification.data.url);
+  }
 });
 
-self.addEventListener('notificationclick', function(event) {
-  console.log('[Service Worker] Notification click Received.');
-
-  event.notification.close();
-
+self.addEventListener('notificationclose', function (event) {
+  'use strict';
   event.waitUntil(
-    clients.openWindow('https://enty.tv/')
+    Promise.all([
+    ])
   );
 });
